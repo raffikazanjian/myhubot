766291ebcb8d333c8bf599576adfe4b6d27b8bf0
Raffi Kazanjian <raffik@hotmail.com>
2016-05-18 09:41:50 -0700

    initial commit for hubot

--- scripts/botCommands.js
+++ scripts/botCommands.js
@@ -1,3 +1,22 @@
+//hear is the bot just listening to something, not dependant on an @, if the text is typed it sends
+/*module.exports = function(robot) {
+	robot.hear(/These boots/, function(response){
+		return response.send('...are made for walkin');
+	})
+}*/
+
+//respond is a call to the bot so you bot responds
+/*module.exports = function(robot) {
+	robot.respond(/Hi Nancy! My name is (.*)/i, function(message){
+		var name = message.match[1];
+		if (name == 'Nancy') {
+			return message.send('You are not Nancy--I am Nancy!');
+		} else {
+			return message.reply('Nice to meet you, ' + name + '!');
+		}
+	})
+}*/
+
 module.exports = function(robot) {
 	
 }
